c7455bd42cfd579ba4b267f382b22c8402b5786a
Julian Andrade <julifeli123@gmail.com>
2023-01-31 08:59:18 -0500

    agregando configuraciónes generales de la app, configuraciónes con tailwind design

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,15 @@
 import MainLayout from '@layout/MainLayout';
 import '@styles/tailwind.css';
 import { Main } from 'next/document';
-
+import { ProviderAuth } from 'hooks/useAuth';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      ´<ProviderAuth>  
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+       </ProviderAuth> 
     </>
   )
 }
---
+++ src/services/api/index.js
@@ -0,0 +1,35 @@
+// estructura para organizar la api
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
+
+const endPoints = {
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
+  products: {
+    getList: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    postProduct: `${API}/api/${VERSION}/products`,
+    putProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    deleteProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+  },
+  users: {
+    getList: (limit) => `${API}/api/${VERSION}/users?limit=${limit}`,
+    postUser: `${API}/api/${VERSION}/users`,
+    isAvailable: `${API}/api/${VERSION}/users/is-available`,
+  },
+  categories: {
+    getList: (limit) => `${API}/api/${VERSION}/categories?limit=${limit}`,
+    postCategory: `${API}/api/${VERSION}/categories`,
+    getCategory: (id) => `${API}/api/${VERSION}/categories/${id}`,
+    putCategory: (id) => `${API}/api/${VERSION}/categories/${id}`,
+    getProductsByCategorie: (id) => `${API}/api/${VERSION}/categories/${id}/products`,
+  },
+  files: {
+    upload: `${API}/api/${VERSION}/files/upload`,
+    getFile: (filename) => `${API}/api/${VERSION}/files/${filename}`,
+  }
+};
+
+export default endPoints;
